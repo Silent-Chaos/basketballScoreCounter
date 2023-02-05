b7025150e5d2c38740e756b7ecec77ecb293a102
SilentChaos <avanishm471@gmail.com>
2023-02-05 18:35:11 +0530

    counter added

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-let 
+let scoreH=document.getElementById("hScore")
+let scoreG=document.getElementById("gScore")
+let scoreOn=0
+let onScore=0
+function hone(){
+scoreH.textContent=onScore+=1
+}
+
+function htwo(){
+    scoreH.textContent=onScore+=2
+    }
+    
+function hthree(){
+    scoreH.textContent=onScore+=3
+    }
+
+    
+function gone(){
+    scoreG.textContent=scoreOn+=1
+    }
+    
+function gtwo(){
+    scoreG.textContent=scoreOn+=2
+    }
+    
+function gthree(){
+    scoreG.textContent=scoreOn+=3
+    }
